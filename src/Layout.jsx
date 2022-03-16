bbd68c2c69247de31d2efcc4562532e48591fef4
Sean Durham <sean@sdurham.net>
2022-03-16 10:57:21 -0700

    Updates

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,58 +1,77 @@
 import "./styles.scss";
 import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { useHotkeys } from "react-hotkeys-hook";
 
 export default function Layout() {
-  const [isPressed, setIsPressed] = useState(0);
-  useHotkeys("m", () => setIsPressed(0), { keyup: true });
-  useHotkeys("m", () => setIsPressed(1), { keydown: true });
+  const [showMargins, setShowMargins] = useState(false);
+
+  function handleMarginCheckboxChange(e) {
+    console.log(e.target.checked);
+    setShowMargins(e.target.checked);
+  }
 
   return (
-    <div className={`container ${isPressed ? "show-tracer" : ""}`}>
+    <div className={`container ${showMargins ? "show-tracer" : ""}`}>
       <div className="nav">
-        <p>M pressed: {isPressed}</p>
         <div className="nav-content">
           <h1 className="example-header">Typographical explorations</h1>
 
           <p className="spacer"></p>
-          <h3 className="example-header">Today</h3>
-          <p>
-            <Link to="/today">Task description before revamp</Link>
-          </p>
+          <h3 className="example-header">Paragraph margin</h3>
+          <ul className="nav-list">
+            <li>
+              <Link to="/today">Today - No paragraph spacing</Link>
+            </li>
+            <li>
+              <Link to="/future-p-w-margin">Paragraphs with margins</Link>
+            </li>
+            <li>
+              <Link to="/future-p-wo-margin">
+                Paragraphs without margins <br />
+                (Blank paragraph hieght = line height)
+              </Link>
+            </li>
+            <li>
+              <Link to="/future-blank-p-narrow">
+                Paragraphs without margins <br />
+                (Blank paragraph hieght = 8px)
+              </Link>
+            </li>
+          </ul>
 
           <p className="spacer"></p>
+          <h3 className="example-header">Header size</h3>
+          <ul className="nav-list">
+            <li>
+              <Link to="/future-small-headers">Small headers</Link>
+            </li>
+            <li>
+              <Link to="/future-medium-headers">Medium headers</Link>
+            </li>
+            <li>
+              <Link to="/future-large-headers">Large headers</Link>
+            </li>
+          </ul>
 
-          <h3 className="example-header">Future</h3>
-          <p>
-            <Link to="/future-small-headers">Small headers</Link>
-          </p>
-          <p>
-            <Link to="/future-medium-headers">Medium headers</Link>
-          </p>
-          <p>
-            <Link to="/future-large-headers">Large headers</Link>
-          </p>
+          <p className="spacer"></p>
+          <h3 className="example-header">List & list item margin</h3>
+          <ul className="nav-list">
+            <li>
+              <Link to="/future-lists-w-margin">Lists with margins</Link>
+            </li>
+          </ul>
           <p className="spacer"></p>
 
-          <p>
-            <Link to="/future-p-w-margin">Paragraphs with margins</Link>
-          </p>
-          <p>
-            <Link to="/future-p-wo-margin">
-              Paragraphs without margins (Blank paragraph hieght = line height)
-            </Link>
-          </p>
-          <p>
-            <Link to="/future-blank-p-narrow">
-              Paragraphs without margins (Blank paragraph hieght = 8px)
-            </Link>
-          </p>
-          <p className="spacer"></p>
-
-          <p>
-            <Link to="/future-lists-w-margin">Lists with margins</Link>
-          </p>
+          <div className="nav-annotation">
+            <label>
+              <input
+                type="checkbox"
+                checked={showMargins}
+                onChange={handleMarginCheckboxChange}
+              />{" "}
+              Show margins
+            </label>
+          </div>
         </div>
       </div>
       <div className="task-panel">
